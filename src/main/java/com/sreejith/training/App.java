821277a4723192cf13cc4728f277f2cb06ded73e
sreejithmahesanpersonal <sreejith.mahesan@gmail.com>
2021-12-28 11:02:34 +0530

    Initial commit

---
+++ src/main/java/com/sreejith/training/App.java
@@ -0,0 +1,13 @@
+package com.sreejith.training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
