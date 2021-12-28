5f93a19469da4bf2cfbf094ae2cfa4ed2424d18f
sreejithmahesanpersonal <sreejith.mahesan@gmail.com>
2021-12-28 15:33:46 +0530

    First Selenium Test

--- src/test/java/com/sreejith/training/FirstTest.java
+++ src/test/java/com/sreejith/training/FirstTest.java
@@ -1,2 +1,30 @@
-package com.sreejith.training;public class FirstTest {
+package com.sreejith.training;
+import com.google.common.base.Verify;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+
+@Test
+public class FirstTest {
+
+ public static void main (String [] args) throws InterruptedException {
+ System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+  //WebDriverManager.chromedriver().setup();
+
+  WebDriver driver = new ChromeDriver();
+  driver.get("https://www.facebook.com/");
+  Thread.sleep(4000);
+  String page_Title = driver.getTitle();
+  Assert.assertEquals(page_Title,"Facebook – log in or sign up");
+  driver.quit();
+ }
+
+
+
 }
